4e2d5c1be431c5b7b5bdf9353fc8e609a1e97435
Harsh Patel <patelharsh9797@users.noreply.github.com>
2024-02-01 17:50:02 +0530

    Tailwind @layer Error Fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main>
-      <h1>Hello</h1>
+      <h1 className={"border-2 border-border p-4"}>Hello</h1>
     </main>
   );
 }
