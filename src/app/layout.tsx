7af5c8eeea9d10f6f797cfb2bc10d87b47e89bfb
Harsh Patel <patelharsh9797@users.noreply.github.com>
2024-02-08 18:17:53 +0530

    Adding auth routes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { GeistSans } from "geist/font/sans";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +15,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider appearance={{ baseTheme: dark }}>
+      <html lang="en">
+        <body className={GeistSans.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
   return (
     <main>
-      <h1 className={"border-2 border-border p-4"}>Hello</h1>
+      <h1 className={"border-2 border-border p-4 text-pink-500"}> Hello</h1>
+      <h2 className="text-2xl border-2 border-black">Hey!!</h2>
     </main>
   );
 }
